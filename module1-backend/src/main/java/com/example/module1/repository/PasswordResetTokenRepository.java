459492012e835cfc2cbc317c85d58f5f86b6a19c
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-07-14 15:44:47 +0100

    Add files via upload

--- module1-backend/src/main/java/com/example/module1/repository/PasswordResetTokenRepository.java
+++ module1-backend/src/main/java/com/example/module1/repository/PasswordResetTokenRepository.java
@@ -1,9 +1,14 @@
-package com.example.module1.repository;
+package backend.authModule.repository;
 
-
-import com.example.module1.entities.PasswordResetToken;
+import backend.authModule.entities.AppUser;
+import backend.authModule.entities.PasswordResetToken;
+import backend.authModule.entities.ProfessionnelSante;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     PasswordResetToken findByToken(String token);
+
+    PasswordResetToken findByUser(AppUser user);
 }
