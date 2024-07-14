6c8e9f7b7d47b3c05f4f4aca9d099703940a8e6b
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-07-14 16:02:02 +0100

    Add files via upload

---
+++ module1-backend/src/main/java/com/example/module1/entities/ProfessionnelSante.java
@@ -0,0 +1,28 @@
+package backend.authModule.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class ProfessionnelSante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cin;
+    private String inpe;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private AppUser user;
+    private boolean confirmed =false;
+    private Boolean isFirstAuth = true;
+
+    private String ROLE="PROFESSIONELSANTE";
+
+
+}
