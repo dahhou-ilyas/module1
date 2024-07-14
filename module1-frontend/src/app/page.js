9a1a25610df4c1b0b4b31cf9e48fc24ffc2b5262
Elias D <smooth.criminal5698@gmail.com>
2024-07-14 11:50:20 +0000

    added live caroussel

--- module1-frontend/src/app/page.js
+++ module1-frontend/src/app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import WelcomingText from "@/components/WelcomingText";
+import LiveCaroussel from "@/components/LiveCaroussel";
 
 export default function Home() {
   return (
     <>  
     <Navbar />
     <WelcomingText />
+    <LiveCaroussel />
     </>
   );
 }
--- module1-frontend/src/components/WelcomingText.jsx
+++ module1-frontend/src/components/WelcomingText.jsx
@@ -1,4 +1,4 @@
-import Illustration from "../../public/image.png";
+import Illustration from "../../public/illustration.png";
 import Image from "next/image";
 
 const WelcomingText = () => {
@@ -9,7 +9,7 @@ const WelcomingText = () => {
                     Vous avez la main sur votre santé
                 </span>
                 <h1 className="text-3xl font-bold my-2 md:text-4xl">Bonjour Ahmed M.</h1>
-                <p className="text-gray-600 mb-8">Mon e-Espace Santé Jeunes (e-ESJ) te permet de consulter ton dossier médical, de profiter de soutiens et tests psychologiques, et de trouver plein d'infos utiles sur la santé.</p>
+                <p className="text-gray-600 mb-6">Mon e-Espace Santé Jeunes (e-ESJ) te permet de consulter ton dossier médical, de profiter de soutiens et tests psychologiques, et de trouver plein d'infos utiles sur la santé.</p>
                 <div className="sm:hidden max-w-[350px] mx-auto">
                     <Image 
                         src={Illustration} 
@@ -17,10 +17,10 @@ const WelcomingText = () => {
                     />
                 </div>
                 <div className="flex flex-col gap-4 sm:flex-row lg:gap-8 justify-start">
-                    <button className="bg-blue-900 hover:bg-blue-800 text-white font-medium md:text-lg rounded-3xl py-2 px-12 mx-6 sm:mx-0">Mon Dossier Medical</button>
-                    <button className="bg-lime-600 hover:bg-lime-500 text-white font-medium md:text-lg rounded-3xl py-2 px-12 mx-6 sm:mx-0">Tests Psychologiques</button>
+                    <button className="bg-blue-900 hover:bg-blue-800 text-white font-medium md:text-lg rounded-3xl py-2 px-10 mx-6 sm:mx-0">Mon Dossier Medical</button>
+                    <button className="bg-lime-600 hover:bg-lime-500 text-white font-medium md:text-lg rounded-3xl py-2 px-10 mx-6 sm:mx-0">Tests Psychologiques</button>
                 </div>
-                <h1 className="text-3xl font-semibold mt-8 md:text-3xl">A la une:</h1>
+                <h1 className="text-3xl md:text-4xl font-bold mt-12">A la une:</h1>
             </div>
             <div className="hidden lg:block animate-bounce-slow xl:-mt-4 -z-10">
                 <div className="max-w-full max-h-[300px] mx-auto">
